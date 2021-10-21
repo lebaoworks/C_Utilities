47cfd51078e77b3e786bdfa95e899b5aadc77e45
lebaoworks <lebaoworks@gmail.com>
2021-10-22 02:37:53 +0700

    add protection

--- client.c
+++ client.c
@@ -2,10 +2,13 @@
 #include "util/queue.h"
 #include "util/unix_socket.h"
 
+#define endpoint "/var/tmp/uds_socket/socket"
+#define sock_dir "/var/tmp/uds_socket"
+
 int main()
 {
     int fd = UDSocket.create();
-    if (!UDSocket.connect(fd, "uds_server"))
+    if (!UDSocket.connect(fd, endpoint))
         printf("Failed to connect.");
     return 0;
 }
--- server.c
+++ server.c
@@ -6,6 +6,9 @@
 #include <errno.h>
 #include "util/unix_socket.h"
 
+#define endpoint "/var/tmp/uds_socket/socket"
+#define sock_dir "/var/tmp/uds_socket"
+
 void authenticate(int fd)
 {
     UDSocketCred cre;
@@ -17,10 +20,11 @@ void authenticate(int fd)
 int main()
 {
     int fd = UDSocket.create();
-    if (!UDSocket.listen_on(fd, "uds_server", 5))  {
+    if (!UDSocket.listen_on(fd, endpoint, 5))  {
         printf("Failed to listen_on. %d", errno);
         return 1;
     }
+    system("chmod 555 /var/tmp/uds_socket");
 
     bool stop=false;
     UDSocket.service(fd, &stop, authenticate);
